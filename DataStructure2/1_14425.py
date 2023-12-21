bc1f325ff356dc3e1afc05e78c4fb877de1a876b
kdh4970 <kdh4970@gmail.com>
2023-12-21 21:07:59 +0900

    update

--- DataStructure2/1_14425.py
+++ DataStructure2/1_14425.py
@@ -1,1 +1,15 @@
 # 문자열 집합
+
+import sys
+
+def input():return sys.stdin.readline().rstrip()
+
+N,M = list(map(int,input().split()))
+
+S = [input() for _ in range(N)]
+
+cnt = 0
+for _ in range(M):
+    if input() in S:
+        cnt += 1
+print(cnt)
