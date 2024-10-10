a3699cbc8700f6ef3983fcb901d2ed0ffc787d72
kdh4970 <kdh4970@gmail.com>
2024-10-10 18:08:37 +0900

    update

--- SWEA/01_5658_보물상자 비밀번호.py
+++ SWEA/01_5658_보물상자 비밀번호.py
@@ -1,8 +1,42 @@
 # 1645 ~
-
-
+from binhex import binhex
 # 회전가능
 # 한면의 모든 숫자는 하나의 숫자의 각 자리수를 의미 함.(16진수)
 # N을 queue에 담아서 로테이트 하며, 가능한 모든수 저장. 단 문자열 0x 뒤에 숫자 붙이고 hex함수
 
-T=
+
+
+from collections import deque
+
+T=int(input().rstrip( ))
+
+vals=set()
+def rotate(q, start, end):
+    if start == end:
+        return
+    for _ in range(4):
+        str_num=list(q)[end*_:end*(_+1)]
+        str_num = "".join(str_num)
+        print(str_num)
+        str_num = int(str_num,16)
+        if str_num not in vals:
+            vals.add(str_num)
+
+    q.rotate(1)
+    rotate(q,start+1,end)
+
+
+
+
+for case in range(T):
+    n,k = map(int,input().rstrip().split(" "))
+    str_numbers=input().rstrip()
+    q=deque([x for x in str_numbers])
+    vals=set()
+    rotate(q,0, n//4)
+    print(vals)
+    # print("\n result \n")
+    vals=list(vals)
+    vals=sorted(vals,reverse=True)
+    # print(vals)
+    print(vals[k])
